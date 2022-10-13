950cdb1c8228440b1e7e7104416068ad19a72f85
wsKim1992 <dntjr9214@gmail.com>
2022-10-13 22:29:08 +0900

    22_10_13

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import * as bootStrap from './plugin/bootstrap/js/bootstrap';
 import './css/font-awesome.css';
 import './css/painting.css';
+/* import './css/portrait.css';*/
+import './css/landscape.css'; 
 import './plugin/bootstrap/css/bootstrap.css';
